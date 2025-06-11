cab274ef849f8ede8d17f660e8e28bfd932fc22c
Achyuta-Dev <achyutareddy.dev@gmail.com>
2025-06-11 08:39:47 +0530

    firstDay class

---
+++ basics/numbers.py
@@ -0,0 +1,47 @@
+#definition: datatypes: what kind of the data you are storing (type of data)
+# Datatype: Numbers
+
+# Integer type
+num1 = 10
+num2 = -20
+print(num1, num2)
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+# Float type
+num3 = 999.99
+print(num3 / num2)
+# Complex type
+# 1+2i mathematically
+comp = 2j
+print(comp)
+comp = 3+2j
+print(comp)
+# Expressions
+# e=mc2
+m,c = 2,1
+e = m*c**2
+print(e)
+# operators
+salary1 = 7800
+salary2 = 2200
+print(salary1, salary2)
+# Arithmetic operators
+print(salary1+salary2)
+print(salary1-salary2)
+print(salary1*salary2)
+print(salary1/salary2)
+print(salary1//salary2)
+print(salary1%salary2)
+# assignment operators
+x = 10
+x+=1
+print(x)
+# operators: num1, num2 these are the operands and + or -, ...etc.
+# same memory
+x = 10
+y = 10
+print(id(x),id(y))
+z = 10
+print(id(z))
+
---
+++ basics/variables.py
@@ -0,0 +1,26 @@
+bottle = "water"
+
+firstname = "cincinatti"
+
+ifs = "india"
+
+#Must start a with letter or underscore
+
+_lastname = "usa"
+#123digits = "1234"
+digits12 = "12345"
+
+#Special symbols are not allowed except _
+#$@
+
+#Case-sensitive
+SUMM = 3
+#print(summ)
+sum = 4
+
+# this is python automation project single line
+"""
+Multiline
+    comments
+"""
+
